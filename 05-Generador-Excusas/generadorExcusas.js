936d66756b863f7245a3eed209569e2aa9949e90
César Guerra <warderer@gmail.com>
2026-04-22 09:42:31 -0600

    Arreglo de Generador de Excusas

---
+++ 05-Generador-Excusas/generadorExcusas.js
@@ -0,0 +1,38 @@
+const columnaA = [
+  "En cuanto se me cayó el internet",
+  "En cuanto terminé de entender el tema",
+  "En cuanto se descompuso mi laptop",
+  "Apenas me responda el asesor",
+  "Cuando por fin se sincronice el equipo",
+  "Cuando logre recuperar el archivo",
+  "En cuanto se calme la carga de materias",
+  "Apenas termine las demás entregas",
+  "Cuando arregle el problema con la plataforma",
+  "En cuanto tenga acceso a la biblioteca digital"
+];
+
+const columnaB = [
+  "del documento final",
+  "del archivo de respaldo",
+  "del proyecto en equipo",
+  "del código fuente",
+  "de la simulación",
+  "de la investigación",
+  "de los resultados",
+  "de la presentación",
+  "del repositorio",
+  "del archivo comprimido"
+];
+
+const columnaC = [
+  "que no se guardó correctamente",
+  "que se corrompió al exportar",
+  "que solo tenía el compañero",
+  "que quedó en otra computadora",
+  "que no sube al sistema",
+  "que la plataforma no reconoce",
+  "que estaba en la nube y desapareció",
+  "que se envió vacío",
+  "que quedó en versión preliminar",
+  "que pide un formato distinto"
+];
